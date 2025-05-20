8dbd1ebf2a42a21c2ead95601f595f540220a7eb
Chillbok <45466997+Chillbok@users.noreply.github.com>
2025-05-20 23:45:52 +0900

    마우스 커서 위치 저장하는 스크립트 생성

--- Assets/Scripts/CursorController.cs
+++ Assets/Scripts/CursorController.cs
@@ -1,11 +1,19 @@
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class CursorController : MonoBehaviour
 {
+    [SerializeField] private Camera mainCamera; //메인카메라
+    private Vector2 mousePos; //마우스 위치
     void Start()
     {
         //게임 시작 시 마우스 커서 보이기
         CursorVisible();
+    }
+
+    void Update()
+    {
+        CursorLocation();
     }
 
     void CursorVisible() //마우스 커서 보이게 하는 메서드
@@ -13,5 +21,12 @@ public class CursorController : MonoBehaviour
         //마우스 커서 보이기
         Cursor.visible = true;
         Cursor.lockState = CursorLockMode.None;
+    }
+
+    void CursorLocation() //마우스 커서 위치 저장
+    {
+        //마우스 위치를 월드 좌표로 변환
+        mousePos = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        Debug.Log(mousePos);
     }
 }
