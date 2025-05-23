43e7724a7ec1cb97ea799c5a3fec587d2e7df523
Chillbok <45466997+Chillbok@users.noreply.github.com>
2025-05-23 23:51:14 +0900

    마우스 위치에 따라 플레이어 공격 애니메이션 방향 다르게 출력하기 완성

--- Assets/Scripts/PlayerAttackController.cs
+++ Assets/Scripts/PlayerAttackController.cs
@@ -1,8 +1,7 @@
-using Unity.VisualScripting;
+using System.Collections;
 using UnityEngine;
-using UnityEngine.AI;
 
-public class PlayerAttack : MonoBehaviour
+public class PlayerAttackController : MonoBehaviour
 {
     [Header("공격 속도 변수")]
     [SerializeField] private float attackCooldown; //공격 쿨타임
@@ -15,24 +14,21 @@ public class PlayerAttack : MonoBehaviour
     [Header("스크립트들")]
     [SerializeField] private PlayerController playerController; //PlaerController.cs 참조변수
     [SerializeField] private CursorController cursorController; //CursorController.cs 참조변수
-    private float lastAttackTime; //마지막 공격 시간
 
     private float angle; //플레이어 - 마우스 각도
     Vector2 mousePosition; //CursorController.cs에서 불러온 마우스 위치
     Vector2 playerPosition; //PlayerController에서 받아온 플레이어 위치
 
     private Animator animator; //애니메이터 컴포넌트를 담을 변수
-    private SpriteRenderer varSpriteRenderer; //스프라이트렌더러 컴포넌트의 참조 변수
+    private SpriteRenderer spriteRenderer; //스프라이트렌더러 컴포넌트의 참조 변수
     private bool AbleToAttack = true; //공격 가능한지 확인하기 위한 변수. true면 공격 가능
-    private bool animationFlipStatus; //현재 플레이어 애니메이션 뒤집어짐 여부
 
     void Awake()
     {
         //초기화
         playerPosition = playerController.currentPlayerPosition; //플레이어 위치 초기화
         animator = GetComponent<Animator>(); //애니메이터 컴포넌트 초기화
-        animationFlipStatus = playerController.animationFlip; //애니메이션 뒤집어짐 여부 초기화
-        varSpriteRenderer = GetComponent<SpriteRenderer>(); //스프라이트렌더러 컴포넌트 초기화
+        spriteRenderer = GetComponent<SpriteRenderer>(); //스프라이트렌더러 컴포넌트 초기화
     }
 
     void Update()
@@ -41,6 +37,7 @@ public class PlayerAttack : MonoBehaviour
         SetPlayerPosition(); //플레이어 위치 업데이트
         CalculateMouseAngle(); //플레이어 - 마우스 각도 계산
         //Debug.Log($"플레이어 위치: {playerPosition}, 마우스 위치: {mousePosition}");
+        Debug.Log("캐릭터에서 마우스까지의 각도: " + angle); //마우스 위치 각도 계산 출력용
         AttackWhenClickButton(); //좌클릭 누를 때 공격 실행 시작
     }
 
@@ -49,6 +46,7 @@ public class PlayerAttack : MonoBehaviour
         if (playerController != null)
         {
             playerPosition = playerController.currentPlayerPosition;
+            playerPosition.y += 1f; //캐릭터 위치 맞추기 위함, y로 1f만큼 올리기
         }
         else
         {
@@ -76,83 +74,62 @@ public class PlayerAttack : MonoBehaviour
         float angle360 = (signedAngle < 0) ? (signedAngle + 360) : signedAngle; //만약 3사분면, 4사분면이면 360 더해서 양수로 만듬
         angle = angle360; //전역변수에 계산한 각도 대입
 
-        //Debug.Log("캐릭터에서 마우스까지의 각도: " + angle); //마우스 위치 각도 계산 출력용
     }
 
     /*
     공격 절차
     1. 플레이어 입력 확인
-    2. 플레이어 쿨타임 확인
+    2. 플레이어 마우스 위치 확인
+    3. 플레이어 마우스 위치에 따라 다른 공격 모션 출력
     */
     void AttackWhenClickButton() //좌클릭 눌렀는지 확인
     {
-        if (Input.GetMouseButtonDown(0)) //마우스 클릭 여부 확인 | true: 마우스 누름
+        if (Input.GetMouseButtonDown(0)) //마우스 클릭 여부 확인
         {
-            TryAttack(); //공격 실행 가능 여부 확인 후 공격 실행
-            //Debug.Log("마우스 좌클릭 확인됨");
-        }
-        else
-        {
+            TryAttack(); //공격 가능 여부 확인
         }
     }
 
     void TryAttack() //공격 가능 여부 확인하는 메서드
     {
-        bool isAttacking = animator.GetCurrentAnimatorStateInfo(0).IsName("Up_Attack_2");
-        bool isAttacking2 = animator.GetCurrentAnimatorStateInfo(0).IsName("Right_Attack_2");
-        bool isAttacking3 = animator.GetCurrentAnimatorStateInfo(0).IsName("Down_Attack_2");
-        //쿨타임 체크
-        if (!isAttacking || !isAttacking2 || !isAttacking3) //공격 애니메이션 아무것도 실행 중이 아니라면
+        //공격 불가능할 때 함수 실행 X
+        if (!AbleToAttack)
         {
-            //Debug.Log("공격 가능");
-            PerformAttack();
-            lastAttackTime = Time.time; //마지막 공격 시간 업데이트
+            return;
         }
-        else
+
+        //애니메이션이 이미 실행 중일 때 함수 실행 X
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("Attack"))
         {
-            Debug.Log("공격 불가능");
+            return;
         }
-    }
 
-    void PerformAttack() //공격 실행하는 메서드
-    {
-        //공격 시작 전
-        animationFlipStatus = varSpriteRenderer.flipX; //현재 플레이어 뒤집혀짐 여부 확인
-
-        //공격 도중
-        AttackAnimationParameter(); //플레이어 - 마우스 각도 계산
+        AttackAnimationParameter(); //애니메이션 패러미터 설정
         animator.SetTrigger("Attack");
-        Debug.Log("공격 애니메이션 출력");
-
-        //공격 후
-        Debug.Log("공격 애니메이션 종료");
-
-        varSpriteRenderer.flipX = animationFlipStatus; //플레이어 뒤집힘 여부 원래대로 돌리기
+        //Debug.Log("공격 애니메이션 실행");
     }
 
     void AttackAnimationParameter() //공격 애니메이션 출력 방향 패러미터 설정
     {
-        string animationParamName = "AttackWay"; //애니메이션 방향 패러미터 이름
-
         if (45f <= angle && angle < 135f) //위쪽
         {
-            animator.SetInteger(animationParamName, 0);
-            varSpriteRenderer.flipX = false;
+            spriteRenderer.flipX = false;
+            animator.SetFloat("AttackWay", 0f);
         }
         else if (135f <= angle && angle < 225f) //왼쪽
         {
-            animator.SetInteger(animationParamName, 1);
-            varSpriteRenderer.flipX = true;
+            spriteRenderer.flipX = true;
+            animator.SetFloat("AttackWay", 1f);
         }
         else if (225f <= angle && angle < 315f) //아래쪽
         {
-            animator.SetInteger(animationParamName, 2);
-            varSpriteRenderer.flipX = false;
+            spriteRenderer.flipX = false;
+            animator.SetFloat("AttackWay", 2f);
         }
         else //오른쪽
         {
-            animator.SetInteger(animationParamName, 1);
-            varSpriteRenderer.flipX = false;
+            spriteRenderer.flipX = false;
+            animator.SetFloat("AttackWay", 1f);
         }
     }
 }
